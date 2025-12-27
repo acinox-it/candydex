3a47bfd5abbc5e68873f4db79591d58305525211
acinox-it <sahraoui.abdeldjalil@outlook.com>
2025-12-27 08:11:17 +0100

    Add Inertia app and SSR setup for server-side rendering

---
+++ inertia/app/app.tsx
@@ -0,0 +1,31 @@
+/// <reference path="../../adonisrc.ts" />
+/// <reference path="../../config/inertia.ts" />
+
+import '../css/app.css'
+import { hydrateRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+
+const appName = import.meta.env.VITE_APP_NAME || 'CandyDex'
+
+createInertiaApp({
+  progress: { 
+    color: '#FF758C',
+    showSpinner: true,
+  },
+
+  title: (title) => `${title} | ${appName}`,
+
+  resolve: (name) => {
+    return resolvePageComponent(
+      `../pages/${name}.tsx`,
+      import.meta.glob('../pages/**/*.tsx'),
+    )
+  },
+
+  setup({ el, App, props }) {
+    
+    hydrateRoot(el, <App {...props} />)
+    
+  },
+});
